27b433f70e30b49f0267b387ce1a047158fbbb1b
Ara <ayaghsizian@gmail.com>
2017-12-04 20:27:16 -0500

    on Form1 setting settings game speed and new game

--- snakeCSharp/Form1.Designer.cs
+++ snakeCSharp/Form1.Designer.cs
@@ -28,9 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.pbCanvas = new System.Windows.Forms.PictureBox();
             this.Score = new System.Windows.Forms.Label();
             this.lblScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
             this.SuspendLayout();
             // 
@@ -65,7 +67,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 631);
+            this.ClientSize = new System.Drawing.Size(973, 549);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.Score);
             this.Controls.Add(this.pbCanvas);
@@ -82,6 +84,7 @@
         private System.Windows.Forms.PictureBox pbCanvas;
         private System.Windows.Forms.Label Score;
         private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- snakeCSharp/Form1.cs
+++ snakeCSharp/Form1.cs
@@ -12,9 +12,23 @@ namespace snakeCSharp
 {
     public partial class Form1 : Form
     {
+        private List<Circle>Snake = new List<Circle>();
+        private Circle food = new Circle();
+
         public Form1()
         {
             InitializeComponent();
+
+            // Set settings to default
+            new Settings();
+
+            // Set game speed and start timer
+            gameTimer.Interval = 1000 / Settings.Speed;
+            gameTimer.Tick += UpdateScreen();
+            gameTimer.Start();
+
+            // Start new game
+            StartGame();
         }
     }
 }

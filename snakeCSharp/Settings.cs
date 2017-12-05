b2ed4d91a52184c8b1d0278581390e329362ccf1
Ara <ayaghsizian@gmail.com>
2017-12-04 20:13:25 -0500

    added settings class which sets initial parameters for the game

---
+++ snakeCSharp/Settings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snakeCSharp
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+
+    class Settings
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Speed { get; set; }
+        public int Score { get; set; }
+        public int Points { get; set; }
+        public bool GameOver { get; set; }
+        public static Direction direction { get; set; }
+
+        public Settings()
+        {
+            Width = 16;
+            Height = 16;
+            Speed = 16;
+            Score = 0;
+            Points = 9;
+            GameOver = false;
+            direction = Direction.Down;
+
+        }
+    }
+}
--- snakeCSharp/input.cs
+++ snakeCSharp/input.cs
@@ -8,12 +8,12 @@ using System.Windows.Forms;
 
 namespace snakeCSharp
 {
-    class input
+    internal class input
     {
         // Load list of available Keyboard buttons
         private static Hashtable keyTable = new Hashtable();
 
-        // Perform a check to see if a particular button is pressed.
+        // Perform a check to see if a particular button is pressed
         public static bool KeyPressed(Keys key)
         {
             if (keyTable[key] == null)
@@ -23,6 +23,10 @@ namespace snakeCSharp
             return (bool) keyTable[key];
         }
 
-        
+        // Detect if a keyboard button is pressed
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
     }
 }
